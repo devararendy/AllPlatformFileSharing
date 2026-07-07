6ae9c78e8e3f7e3887676c438f0c0c75c80b8a92
Rendy Devara <rendy_devara@msn.com>
2026-07-07 22:04:07 +0700

    added requirement and auto install

--- share.sh
+++ share.sh
@@ -2,8 +2,12 @@
 SCRIPT_PATH=$(realpath "${BASH_SOURCE[0]}")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 echo "Absolute script directory: $SCRIPT_DIR"
+python -m pip install -r requirements.txt
 
 PORT=8000
 
-cd ~/storage/dcim/sharing
+# change the share directory to your desired location
+SHARE_DIR=~/
+
+cd $SHARE_DIR
 python3 $SCRIPT_DIR/share.py $PORT
